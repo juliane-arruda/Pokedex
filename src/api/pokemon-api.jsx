c15655ae4cf8ef61e45839cf795495d1db47a638
Juliane Arruda <juliane_ch.arruda@live.com>
2020-03-01 20:53:13 -0300

    add visualização de pokemons para 1000

--- src/api/pokemon-api.jsx
+++ src/api/pokemon-api.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const pokemonList = () => {
-  return axios.get('https://pokeapi.co/api/v2/pokemon');
+  return axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000');
 }
