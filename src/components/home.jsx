38fe172d4bd8447c5d8e356c4c53221bc96d13af
Juliane Arruda <juliane_ch.arruda@live.com>
2020-03-02 03:46:47 -0300

    add imagens dos pokemons

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -37,7 +37,7 @@ class Home extends Component {
       <div>
         <input value={this.state.filter} onChange={this.setFilter} />
         <ul>
-          {this.state.filterList.map((elem, position) => <li>{elem.name} {position + 1}</li>)}
+          {this.state.filterList.map((elem, position) => <li> <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${(position + 1).toString().padStart(3,'0')}.png`} alt="" />{elem.name} {position + 1}</li>)}
         </ul>
       </div>
     )
