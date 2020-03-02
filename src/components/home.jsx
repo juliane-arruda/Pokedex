47d0052ac6e7e84db7843731951b437bf15e943e
Juliane Arruda <juliane_ch.arruda@live.com>
2020-03-01 21:24:09 -0300

    add filtro simples para lista de pokemons

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
-import {pokemonList} from '../api/pokemon-api';
+import { pokemonList } from '../api/pokemon-api';
 
 class Home extends Component {
-  state = {list: []}
-  
-  async componentDidMount(){
-    const list = await pokemonList();
+  state = {
+    list: [],
+    filter: '',
+    filterList: [],
+  }
 
+  setFilter = (e) => {
     this.setState({
-      list : list.data.results,
+      filter: e.target.value,
+    }, this.applyFilter)
+  }
+
+  applyFilter = () => {
+    this.setState({
+      filterList: this.state.list.filter((pokemon) => pokemon.name.indexOf(this.state.filter) !== -1)
     })
   }
 
+  async componentDidMount() {
+    const list = await pokemonList();
+
+    this.setState({
+      list: list.data.results,
+    }, this.applyFilter)
+  }
+
   render() {
-    return(
-      <ul>
-        {this.state.list.map((elem) => <li>{elem.name}</li> )}
-      </ul>
+    return (
+      <div>
+        <input value={this.state.filter} onChange={this.setFilter} />
+        <ul>
+          {this.state.filterList.map((elem) => <li>{elem.name}</li>)}
+        </ul>
+      </div>
     )
   }
 }
