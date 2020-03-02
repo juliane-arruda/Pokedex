a4c8100beb9c20f03b4220cf1c1ed0e0ed7773ff
Juliane Arruda <juliane_ch.arruda@live.com>
2020-03-02 03:45:28 -0300

    add rota details

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Home from './components/home';
+import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
+import Details from './components/details';
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/details/:id" component={Details} />
+      </Switch>
+    </Router>
   );
 }
 
