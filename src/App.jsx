4ab7e51c001f28ea149503419437a9aede0e71cd
Juliane Arruda <juliane_ch.arruda@live.com>
2020-03-03 00:34:56 -0300

    add navbar e footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import Home from './components/home';
 import { Switch, Route } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import './App.css';
 import Details from './components/details';
+import Footer from './components/footer';
+import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/details/:id" component={Details} />
-      </Switch>
-    </Router>
+    <div>
+      <Navbar />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/details/:id" component={Details} />
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
   );
 }
 
