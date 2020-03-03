b867f86322d9d1c4b71b825f381137049e802ad7
Juliane Arruda <juliane_ch.arruda@live.com>
2020-03-03 00:32:45 -0300

    add style de rodapé

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./footer.css"
+
+const Footer = () => (
+  <footer className='footer mt-auto py-3 color'>
+    <div className="footer-copyright text-center">
+      <h5>
+        © 2020 Copyright: Feito Por: Juliane
+      </h5>
+    </div>
+
+  </footer>
+)
+
+export default Footer;
