3a915cd5db6c7a7b56a6ae354d87547ebd9c466e
Juliane Arruda <juliane_ch.arruda@live.com>
2020-03-03 00:29:52 -0300

    removido img-logo e direcionada para navbar

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { pokemonList } from '../api/pokemon-api';
 import FuzzySearch from 'fuzzy-search';
+import './home.css';
 
 class Home extends Component {
   state = {
@@ -34,9 +35,8 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
-        <img className="img-fluid img-thumbnail" src="/images/pokemon-logo.png" alt="Pokémon-logo" />
-        <input class="form-control form-control-lg col-12" value={this.state.filter} onChange={this.setFilter} placeholder="Pesquise por um Pokémon" />
+      <div className="container-fluid home-page">
+        <input className="form-control form-control-lg col-12" value={this.state.filter} onChange={this.setFilter} placeholder="Pesquise por um Pokémon" />
         {/* <div>
           {this.state.filterList.map((elem, position) =>
             <div>
@@ -44,19 +44,20 @@ class Home extends Component {
             </div>)}
         </div> */}
 
-        <div class="card" style={{ width: '18rem' }}>
+        <section className="home-card">
           {this.state.filterList.map((elem, position) =>
-            <>
-              <div>
-                <img class="card-img-top" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${(position + 1).toString().padStart(3, '0')}.png`} alt="Pokémon" />
-              </div>
-              <div class="card-body">
-                <p class="card-text">
-                  {elem.name} {position + 1}
-                </p>
-              </div>
-            </>)}
-        </div>
+            <div class="card" style={{ width: '18rem' }}>
+                <div>
+                  <img class="card-img-top" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${(position + 1).toString().padStart(3, '0')}.png`} alt="Pokémon" />
+                </div>
+                <div class="card-body">
+                  <p class="card-text">
+                    {elem.name} {position + 1}
+                  </p>
+                </div>
+              </div>)}
+        </section>
+
 
       </div>
     )
