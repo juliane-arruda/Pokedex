9877fbe16d8bfd87bcbacef6de00cbe5875926b0
Juliane Arruda <juliane_ch.arruda@live.com>
2020-03-03 03:40:36 -0300

    add  style de card add imagem dos pokemons pelo id, add loading e corrigido o número do pokemon

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { pokemonList } from '../api/pokemon-api';
 import FuzzySearch from 'fuzzy-search';
-import './home.css';
+import PokemonImg from './pokemon-img';
+import { Link } from 'react-router-dom';
+import Loading from './loading';
+import './home.scss';
 
 class Home extends Component {
   state = {
-    list: [],
+    list: null,
     filter: '',
     filterList: [],
   }
@@ -22,43 +25,43 @@ class Home extends Component {
     });
     this.setState({
       filterList: searcher.search(this.state.filter)
-    })
+    });
   }
 
   async componentDidMount() {
     const list = await pokemonList();
 
     this.setState({
-      list: list.data.results,
+      list: list.data.results.map((pokemon, position) => ({ ...pokemon, id: position + 1 })),
     }, this.applyFilter)
   }
+
 
   render() {
     return (
       <div className="container-fluid home-page">
-        <input className="form-control form-control-lg col-12" value={this.state.filter} onChange={this.setFilter} placeholder="Pesquise por um Pokémon" />
+        <input className="form-control form-control-lg col-12 d-flex justify-content-center" value={this.state.filter} onChange={this.setFilter} placeholder="Pesquise por um Pokémon" />
+        {this.state.list === null ?
+          <Loading /> :
+          <section className="home-card">
+            {this.state.filterList.map((elem) =>
+              <Link to={`/details/${elem.id}`} className="card m-3" style={{ width: '18rem' }}>
+                <div>
+                  <PokemonImg id={elem.id} />
+                </div>
+                <div className="card-body">
+                  <p className="card-text"><small className="text-muted">N° {elem.id}</small></p>
+                  <h4 className="card-title pokemon-name d-flex justify-content-center">{elem.name}</h4>
+                </div>
+              </Link>)}
+          </section>}
+
         {/* <div>
           {this.state.filterList.map((elem, position) =>
             <div>
               <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${(position + 1).toString().padStart(3, '0')}.png`} alt="Pokémon" /> {elem.name} {position + 1}
             </div>)}
         </div> */}
-
-        <section className="home-card">
-          {this.state.filterList.map((elem, position) =>
-            <div class="card" style={{ width: '18rem' }}>
-                <div>
-                  <img class="card-img-top" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${(position + 1).toString().padStart(3, '0')}.png`} alt="Pokémon" />
-                </div>
-                <div class="card-body">
-                  <p class="card-text">
-                    {elem.name} {position + 1}
-                  </p>
-                </div>
-              </div>)}
-        </section>
-
-
       </div>
     )
   }
